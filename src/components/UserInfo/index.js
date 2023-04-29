8dca051931522be53410a26e36031b8efe3b7872
jhasandeep <skjsan11@gmail.com>
2023-04-29 15:04:15 +0000

    first commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="user-name">SANDEEP KUMAR JHA</h1>
+    <p className="user-designation">Software developer at Delhi</p>
+  </div>
+)
+
+export default UserInfo
